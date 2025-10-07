c642f409c9ee3821326e7a459a18103fcde0c8ab
cheezi0747 <linus.sjolinder@b3.se>
2025-10-07 08:32:38 +0200

    dependabot/maven/main/se.sundsvall.dept44-dept44-service-parent-6.0.13: update api spec

--- src/integration-test/java/se/sundsvall/digitalregisteredletter/openapi/OpenApiSpecificationIT.java
+++ src/integration-test/java/se/sundsvall/digitalregisteredletter/openapi/OpenApiSpecificationIT.java
@@ -1,9 +1,12 @@
 package se.sundsvall.digitalregisteredletter.openapi;
 
+import static java.nio.file.Files.writeString;
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.List;
 import net.javacrumbs.jsonunit.core.Option;
 import org.junit.jupiter.api.Test;
@@ -44,9 +47,12 @@ class OpenApiSpecificationIT {
 	private TestRestTemplate restTemplate;
 
 	@Test
-	void compareOpenApiSpecifications() {
+	void compareOpenApiSpecifications() throws IOException {
 		final String existingOpenApiSpecification = ResourceUtils.asString(openApiResource);
 		final String currentOpenApiSpecification = getCurrentOpenApiSpecification();
+
+		writeString(Path.of("target/generated-api.yaml"), currentOpenApiSpecification);
+
 		assertThatJson(toJson(currentOpenApiSpecification))
 			.withOptions(List.of(Option.IGNORING_ARRAY_ORDER))
 			.whenIgnoringPaths("servers")
